84a10f85c10adc3eeab0ae1f031137c607337706
tyleryoung98 <tyoung23@students.towson.edu>
2019-04-15 21:18:57 -0400

    Fixing html, added queries and routes

--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,7 @@ module.exports = function(){
     console.log(req.body);
     const{flight_no,tail_no,origin,dest,to_time,land_time,seats} = req.body;
     dbedits.addFlight(flight_no,tail_no,origin,dest,to_time,land_time,seats).then(()=>{
-      res.redirect('/app/company.html');
+      res.redirect('/html/company.html');
     });
   });
 
@@ -14,8 +14,39 @@ module.exports = function(){
     console.log(req.body);
     const{tail_no,type,flight_hours,company,maxTOWeight,maxLandWeight,capacity,maxCargoWeight} = req.body;
     dbedits.addAirplane(tail_no,type,flight_hours,company,maxTOWeight,maxLandWeight,capacity,maxCargoWeight).then(()=>{
-      res.redirect('/app/admin.html');
+      res.redirect('/html/airplane.html');
     });
   });
 
+  router.post('/runway', function(req, res){
+    console.log(req.body);
+    const{airport_Code,runway_ID,length,status} = req.body;
+    dbedits.addFlight(airport_Code,runway_ID,length,status).then(()=>{
+      res.redirect('/html/airport.html');
+    });
+  });
+
+  router.post('/gate', function(req, res){
+    console.log(req.body);
+    const{section,gate_ID,company,airport_Code} = req.body;
+    dbedits.addFlight(section,gate_ID,company,airport_Code).then(()=>{
+      res.redirect('/html/airport.html');
+    });
+  });
+
+  router.post('/pilot', function(req, res){
+    console.log(req.body);
+    const{employee_ID,name,company,flight_hours,position,flight_no} = req.body;
+    dbedits.addFlight(employee_ID,name,company,flight_hours,position,flight_no).then(()=>{
+      res.redirect('/html/flight.html');
+    });
+  });
+
+  router.post('/flightData', function(req, res){
+    console.log(req.body);
+    const{} = req.body;
+    dbedits.addFlight(flight_no,to_weight,fuel,callsign).then(()=>{
+      res.redirect('/html/flightData.html');
+    });
+  });
 }
---
+++ sql.js
@@ -0,0 +1,160 @@
+const sql = require("mysql");
+
+module.exports = class TableEdits{
+//--------------------FLIGHTS UPDATE/INSERT---------------------------
+  addFlight(flight_no,tail_no,origin,dest,to_time,land_time,seats){
+    return new Promise((resolve, reject)=> {
+      var quer= "IF EXISTS (select * from FLIGHTS where flight_no="+flight_no+") "+
+                "UPDATE FLIGHTS SET tail_no="+tail_no+","+
+                                    "origin="+origin+","+
+                                    "dest="+dest+","+
+                                    "to_time="+to_time+","+
+                                    "land_time="+land_time+","+
+                                    "seats="+seats+
+                                    "where flight_no="+flight_no+
+                " ELSE INSERT INTO FLIGHTS (flight_no, tail_no, origin, dest,"
+                                            +" to_time, land_time, seats)"
+                                    +" VALUES ("+flight_no+","+tail_no
+                                    +","+origin+","+dest
+                                    +","+to_time+","+land_time
+                                    +","+seats+")";
+      sql.query(quer, function(err, result){
+        if(err){
+          console.log("Failed to insert into FLIGHTS");
+          reject();
+        }
+        else{
+          console.log("Info inserted into FLIGHTS");
+          resolve();
+        }
+      });
+    });
+  }
+
+//-------------------------AIRPLANE UPDATE/INSERT---------------------------------------
+  addAirplane(tail_no,type,flight_hours,company,maxTOWeight,maxLandWeight,capacity,maxCargoWeight){
+    return new Promise((resolve, reject)=> {
+      var quer= "IF EXISTS (select * from AIRPLANE where tail_no="+tail_no+") "+
+                          "UPDATE AIRPLANE SET type="+type+","+
+                          "flight_hours="+flight_hours+","+
+                          "company="+company+","+
+                          "maxTOWeight="+maxTOWeight+","+
+                          "maxLandWeight="+maxLandWeight+","+
+                          "capacity="+capacity+
+                          "maxCargoWeight="+maxCargoWeight+
+                          "where flight_no="+flight_no+
+                " ELSE INSERT INTO AIRPLANE (tail_no,type,flight_hours,company,"
+                                            +"maxTOWeight,maxLandWeight,"
+                                            +"capacity,maxCargoWeight)"
+                                    +" VALUES ("+tail_no+","+type+","+flight_hours
+                                    +","+company+","+maxTOWeight
+                                    +","+maxLandWeight+","+capacity
+                                    +","+maxCargoWeight+")";
+      sql.query(quer, function(err, result){
+        if(err){
+          console.log("Failed to insert into AIRPLANE");
+          reject();
+        }
+        else{
+          console.log("Info inserted into AIRPLANE");
+          resolve();
+        }
+      });
+    });
+  }
+
+//-------------------------RUNWAY UPDATE/INSERT---------------------------------------
+  addRunway(airport_Code,runway_ID,length,status){
+    return new Promise((resolve, reject)=> {
+      var quer= "IF EXISTS (select * from RUNWAY where airport_Code="+airport_Code+
+                                                  " and runway_ID="+runway_ID+") "+
+                          "UPDATE RUNWAY SET status="+status+","+
+                " ELSE INSERT INTO RUNWAY (airport_Code,runway_ID,length,status)"
+                                +" VALUES ("+airport_Code+","+runway_ID
+                                +","+length+","+status+")";
+      sql.query(quer, function(err, result){
+        if(err){
+          console.log("Failed to insert into RUNWAY");
+          reject();
+        }
+        else{
+          console.log("Info inserted into RUNWAY");
+          resolve();
+        }
+      });
+    });
+  }
+
+//-------------------------GATE UPDATE/INSERT---------------------------------------
+  addGate(section,gate_ID,company,airport_Code){
+    return new Promise((resolve, reject)=> {
+      var quer= "IF EXISTS (select * from GATE where airport_Code="+airport_Code+
+                                                  " and gate_ID="+gate_ID+
+                                                  " and section="+section+") "+
+                          "UPDATE GATE SET status="+status+
+                " ELSE INSERT INTO GATE (section,gate_ID,company,airport_Code)"
+                                +" VALUES ("+section+","+gate_ID
+                                +","+company+","+airport_Code+")";
+      sql.query(quer, function(err, result){
+        if(err){
+          console.log("Failed to insert into GATE");
+          reject();
+        }
+        else{
+          console.log("Info inserted into GATE");
+          resolve();
+        }
+      });
+    });
+  }
+
+//-------------------------PILOT UPDATE/INSERT---------------------------------------
+  addPilot(employee_ID,name,company,flight_hours,position,flight_no){
+    return new Promise((resolve, reject)=> {
+      var quer= "IF EXISTS (select * from PILOT where employee_ID="+employee_ID+") "+
+                          "UPDATE PILOT SET name="+name+","+
+                                            "company="+company+","+
+                                            "flight_hours="+flight_hours+","+
+                                            "position="+position+","+
+                                            "flight_no="+flight_no+","+
+                " ELSE INSERT INTO PILOT (employee_ID,name,company,flight_hours,"
+                                          +"position,flight_no)"
+                                +" VALUES ("+employee_ID+","+name
+                                +","+company+","+flight_hours
+                                +","+position+","+flight_no+")";
+      sql.query(quer, function(err, result){
+        if(err){
+          console.log("Failed to insert into PILOT");
+          reject();
+        }
+        else{
+          console.log("Info inserted into PILOT");
+          resolve();
+        }
+      });
+    });
+  }
+
+//-------------------------GATE UPDATE/INSERT---------------------------------------
+  addFlightData(flight_no,to_weight,fuel,callsign){
+    return new Promise((resolve, reject)=> {
+      var quer= "IF EXISTS (select * from FLIGHTDATA where flight_no="+flight_no+") "+
+                          "UPDATE FLIGHTDATA SET to_weight="+to_weight+","+
+                                                "fuel="+fuel+","+
+                                                "callsign="+callsign+
+                " ELSE INSERT INTO FLIGHTDATA (flight_no,to_weight,fuel,callsign)"
+                                +" VALUES ("+flight_no+","+to_weight
+                                +","+fuel+","+callsign+")";
+      sql.query(quer, function(err, result){
+        if(err){
+          console.log("Failed to insert into FLIGHTDATA");
+          reject();
+        }
+        else{
+          console.log("Info inserted into FLIGHTDATA");
+          resolve();
+        }
+      });
+    });
+  }
+}
