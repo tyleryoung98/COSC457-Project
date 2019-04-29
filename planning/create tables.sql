121a0245ead3578343f26c2308e2e868fe723f92
tyleryoung98 <tyoung23@students.towson.edu>
2019-04-29 18:37:33 -0400

    create ables update

--- planning/create tables.sql
+++ planning/create tables.sql
@@ -13,13 +13,58 @@ create table if not exists RUNWAY(
     Open_Status		varchar(6)	not null,
     foreign key(Airport_Code) references AIRPORT(Airport_Code));
     
+create table if not exists COMPANY(
+	Company_Name	varchar(25)	not null,
+    Location		varchar(30),
+    Phone			char(10),
+    primary key (Company_Name));
+    
 create table if not exists GATES(
 	Airport_Code	varchar(4)	not null,
     Section			char		not null,
     Gate_ID			int			not null,
     Company			varchar(25)	not null,
-    foreign key (Airport_Code) references AIRPORT(Airport_Code));*/
+    foreign key (Airport_Code) references AIRPORT(Airport_Code),
+    foreign key (Company) references COMPANY (Company_Name));
+
+create table  if  not exists AIRPLANE(
+	Tail_no			varchar(8)	not null,
+    Plane_type		varchar(8)	not null,
+	Flight_hours	int,
+    Company			varchar(25),
+    MaxToWeight		int,
+    MaxLandWeight	int,
+    Capacity		int			not null,
+    MaxCargoWeight	int			not null,
+    primary key (Tail_no),
+    foreign key (Company) references COMPANY (Company_Name));
     
+create table  if not exists FLIGHTS(
+	Flight_no		varchar(8)	not null,
+    Tail_no			varchar(8)	not null,
+    Origin			varchar(4)	not null,
+    Dest			varchar(4)	not null,
+    TO_Time			time		not null,
+    Land_Time		time		not null,
+    Seats			int			not null,
+    primary key (Flight_no),
+    foreign key (Tail_no) references AIRPLANE (Tail_no),
+    foreign key (Origin) references AIRPORT (Airport_Code),
+    foreign key (Dest) references AIRPORT (Airport_Code));*/
+
+create table if not exists PILOT(
+	Employee_ID		char(10)	not null,
+    Fname			varchar(15)	not null,
+    Lname			varchar(15) not null,
+    Company			varchar(25) not null,
+    Flight_hours	int,
+    Title			varchar(15) not null,
+    Flight_no		varchar(8)	not null,
+    primary key (Employee_ID),
+    foreign key (Company) references COMPANY (Company_Name),
+    foreign key (Flight_no) references FLIGHTS (Flight_no));
+    
+
 
 
 
